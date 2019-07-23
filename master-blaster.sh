d0894b3d0e1616091ea95c86ceee63e7d117425e
Shere Khan <46861603+phantompacket@users.noreply.github.com>
2019-07-23 03:20:28 -0700

    Update master-blaster.sh

--- master-blaster.sh
+++ master-blaster.sh
@@ -4,13 +4,13 @@ echo "What port would you like to scan?"
 
 read port_var
 
-echo "Press CTRL+C to stop scanning" && masscan 0.0.0.0/0 -p$port_var --exclude 255.255.255.255 --rate 99999 >> out1.txt
+echo "Press CTRL+C to stop scanning" && masscan 0.0.0.0/0 -p$port_var --exclude 255.255.255.255 --rate 99999 >> /root/master-blaster/out1.txt
 
 cat out1.txt | sed '1,5d' >> out2.txt
 
 clear
 
-cat out2.txt | colrm 1 31 >> results.txt
+cat out2.txt | colrm 1 31 >> /root/master-blaster/results.txt
 
 rm out1.txt && rm out2.txt && rm paused.conf
 
@@ -18,4 +18,4 @@ clear
 
 cat results.txt | colrm 20 100
 
-nmap -Pn -sS -F -iL /root/results.txt
+nmap -Pn -sS -F -iL /root/master-blaster/results.txt
